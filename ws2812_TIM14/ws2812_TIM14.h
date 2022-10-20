8257b0d55ad2aa25eb5daf207c15d1cae9d1699a
Oleg <olegreshetnik@gmail.com>
2022-10-20 23:58:19 +0300

    Update ws2812_TIM14.h

--- ws2812_TIM14/ws2812_TIM14.h
+++ ws2812_TIM14/ws2812_TIM14.h
@@ -5,17 +5,17 @@
 //#define WS_GAMMA_TABLE // использовать таблицу гамма коррекции, если не определено, то используем вычисление
 
 // Настройки порта ленты
-#define WS_PORT		    GPIOA // ws port
-#define WS_PIN	    	9 // ws pin
-#define WS_MASK	    	(1 << WS_PIN)
+#define WS_PORT			GPIOA // ws port
+#define WS_PIN			9 // ws pin
+#define WS_MASK			(1 << WS_PIN)
 
-#define WS_LO	19
-#define WS_HI	39
-#define WS_INT	60
+#define WS_LO			19
+#define WS_HI			39
+#define WS_INT			60
 
 
 #define NUM_LEDS		12 // количество светодиодов
-#define NUM_BYTES       (NUM_LEDS*3)
+#define NUM_BYTES		(NUM_LEDS*3)
 
 void WsInit(void);
 void WsSend(void);
@@ -24,3 +24,4 @@ void wsWeel(void);
 void wsFire(void);
 void wsColors(void);
 void wsSparkles(void);
+void wsBugs(void);
